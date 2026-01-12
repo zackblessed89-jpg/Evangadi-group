6dc7b21e90c982b0564bfc0004c3c470c2efc9c7
hana <wondiehana@gmail.com>
2026-01-12 22:26:07 +0300

    imports added on askquestion.jsx

---
+++ frontend/src/Pages/AskQuestion/AskQuestion.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import QuestionForm from "../../features/questions/QuestionForm/QuestionForm";
+import classes from "./AskQuestion.module.css";
+import { FaLightbulb, FaPenFancy, FaCheckCircle, FaEye } from "react-icons/fa";
+import AOS from "aos";
+import "aos/dist/aos.css";
